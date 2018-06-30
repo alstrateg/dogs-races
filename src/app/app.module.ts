b6bef7df7efc646660fb7a999932ddd25bb5a58a
alstrateg <grzegorz.machnik@gmail.com>
2018-06-30 16:43:23 +0200

    Add compoentents mock and rotings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { EditDogComponent } from './edit-dog.component';
+import { ListDogsComponent } from './list-dogs.component';
+import { InfoComponent } from './info.component';
+
+const appRoutes: Routes = [
+  { path: 'dogs/new', component: EditDogComponent },
+  { path: 'dogs/edit/:id', component: EditDogComponent },
+  {
+    path: 'dogs',
+    component: ListDogsComponent,
+    data: { title: 'Dogs List' }
+  },
+  { path: '', component: InfoComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    EditDogComponent,
+    ListDogsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
